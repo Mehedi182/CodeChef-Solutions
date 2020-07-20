c3612620bda290ba4299160bbd5fd03da3e58be2
Mehedi Hasan <58473141+Mehedi182@users.noreply.github.com>
2020-07-20 21:42:44 +0600

    Create Id and Ship.cpp

---
+++ Id and Ship.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+
+    char s;
+    cin>>s;
+    if(s=='B'||s=='b')
+    cout<<"BattleShip"<<endl;
+    else if(s=='C'||s=='c')
+    cout<<"Cruiser"<<endl;
+    else if(s=='D'||s=='c')
+    cout<<"Destroyer"<<endl;
+    else if(s=='F'||s=='f')
+    cout<<"Frigate"<<endl;
+
+}
+  }
